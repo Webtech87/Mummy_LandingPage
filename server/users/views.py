9b4f1f97861bc94560068f64a69b675da14db7a5
boris <boris.isac@webtech87.com>
2025-04-22 14:49:44 +0100

    [UPDATE] Sending form Logic

--- server/users/views.py
+++ server/users/views.py
@@ -1,3 +1,43 @@
+from django.views import View
 from django.shortcuts import render
+from .forms import InformatioinForm
+from django.http import JsonResponse
 
-# Create your views here.
+
+class IndexView(View):
+
+    template_name = 'index.html'
+
+    def get(self, request, *args, **kwargs):
+        form = InformatioinForm()
+        context = {
+            "title": "Index Users",
+            'form': form
+        }
+        return render(request, self.template_name,  context)
+
+    def post(self, request, *args, **kwargs):
+            form = InformatioinForm(request.POST)
+            if form.is_valid():
+                print("form is valid")
+                data = {
+                    'status': 'success',
+                    'message': 'Formulário válido',
+                    'data': {
+                        'nome_completo': form.cleaned_data['full_name'],
+                        'email': form.cleaned_data['email'],
+                        'telefone': form.cleaned_data['phone'],
+                        'assunto': form.cleaned_data['objective'],
+                        'mensagem': form.cleaned_data['question_text'],
+                        'aceite': form.cleaned_data['accept']
+                    }
+                }
+                return JsonResponse(data)
+
+            else:
+                return JsonResponse({
+                    'status': 'error',
+                    'message': "Form does not valide.",
+                    'errors': form.errors
+                }, status=400)
+

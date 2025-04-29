2deef134107fde00d448ad39f8569011542bea65
Felipe Silva <felipe.silva@webtech87.pt>
2025-04-29 10:40:10 +0100

    [UPDATE] Installed whitenoise and gunicorn and set up for deploy

---
+++ server/build.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+set -o errexit
+
+# Change to backend directory
+cd server
+
+# Install dependencies
+pip install -r requirements.txt
+
+# Apply all migrations properly
+python manage.py migrate
+
+# Collect static files
+python manage.py collectstatic --noinput
--- server/server/settings.py
+++ server/server/settings.py
@@ -17,7 +17,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.getenv('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['localhost', '127.0.0.1']
 
@@ -52,6 +52,7 @@ INSTALLED_APPS = [
 MIDDLEWARE = [
     'corsheaders.middleware.CorsMiddleware',  # This should be first or near first
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -152,6 +153,11 @@ RECEPTION_EMAIL = os.getenv('TO_EMAIL_PARSERIA')
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 STATIC_URL = 'static/'
+STATIC_ROOT = BASE_DIR / 'staticfiles'
+STATICFILES_DIRS = [
+    BASE_DIR / 'static',
+]
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
